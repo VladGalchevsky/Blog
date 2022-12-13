3838b5c0bb1148e64151e84180c318f7d2497bab
VladGalchevsky <vladgalchevsky97@gmail.com>
2022-12-13 18:35:28 +0200

    creating models

---
+++ blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=100, verbose_name='Url', unique=True)
+    author = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    views = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT,
+                                 related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='tags')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
