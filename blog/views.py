0245693bdc1309de45006b9dae0c18270146bed7
VladGalchevsky <vladgalchevsky97@gmail.com>
2022-12-15 13:05:34 +0200

    content output

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import *
+
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titte'] = 'Classic Blog Design'
+        return context
 
 
 def index(request):
     return render(request, 'blog/index.html')
+
+
+def get_category(request):
+    return render(request, 'blog/category.html')
+
+
+def get_post(request):
+    return render(request, 'blog/category.html')
