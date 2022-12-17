e614b5a397489a82310ed6b07a57762c4617a6f5
VladGalchevsky <vladgalchevsky97@gmail.com>
2022-12-17 16:34:49 +0200

    output of posts and tags

--- blog/views.py
+++ blog/views.py
@@ -46,4 +46,15 @@ class GetPost(DetailView):
 
 
 class PostByTag(ListView):
-    pass
+    template_name = 'blog/category_page.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titte'] = str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
