5508327f6acf7e1e9fb888499469421663226e19
VladGalchevsky <vladgalchevsky97@gmail.com>
2022-12-18 12:24:21 +0200

    migrate from SQLite to PostgreSQL

--- siteblog/settings.py
+++ siteblog/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'siteblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog_db',
+        'USER': 'blog_db',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
